805e524ae686ab8b0204c3e7e92f4a75207ccd41
cavanaghb <briancavanagh99@gmail.com>
2015-07-24 11:40:34 +0100

    v2

--- selectfilescreen.py
+++ selectfilescreen.py
@@ -1,20 +1,39 @@
+
 __author__ = 'cavanaghb'
+
 
 from tkinter import *
 
 class selectfile:  #class for first screen
 
-        root = Tk()  # create the object "root" from the constructor Tk(), Tk() is a Tkinter class
-        root.geometry("1000x600+200+50") #main box position
-        theLabel = Label(root, text="Select Screen")
-        theLabel.pack()  # packs the text from the previous line into the screen
 
-        fileselectbox = askopenfile()   #create a file browser and selection area/box
+        def __init__(self, master):
 
-        framesize = Frame(height=550, width=950, bd=2, relief=SUNKEN) #internal 'content frame' details
-        framesize.pack(fill=X, padx=5, pady=5)
+            self.filebutton = Button(master, text="Open Files for Selection button", command=self.getfile)
+            self.filebutton.pack()
 
-        root.mainloop()  # this keeps the box open and provides the X or minimize, needs to be in the same 'def' as 'root'
+            self.quitbutton =
+            self.quitbutton.pack()
+
+
+
+        def getfile(self):
+
+            Button = askopenfilename(title="choose your file", filetypes=[("jpeg files", "*.jpg"), ("all files", "*.*")])
+
+
+#BEGIN - CREATE THE MAIN WINDOW
+mainbox = Tk()  # create the object "mainbox" from the constructor Tk(), Tk() is a Tkinter class
+mainbox.geometry("1000x600+200+50") #main box position and size
+theLabel = Label(mainbox, text="MAIN SELECT FILE SCREEN")
+theLabel.pack()  # packs the text from the previous line into the screen
+mainbox.title("File Analysis Tool")
+getfilebutton = selectfile(mainbox)
+#END - CREATE THE MAIN WINDOW
+
+
+mainbox.mainloop()  # this keeps the box open and provides the X or minimize, needs to be in the same 'def' as 'root'
+
 
 
 testrun = selectfile() #THIS IS A TEST RUN OF THIS CLASS
