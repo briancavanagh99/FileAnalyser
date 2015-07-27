c41a02ffc561015248d03b669af89cad1edcc6ab
cavanaghb <briancavanagh99@gmail.com>
2015-07-27 12:48:00 +0100

    v3.4

--- selectfilescreen.py
+++ selectfilescreen.py
@@ -3,13 +3,14 @@ __author__ = 'cavanaghb'
 
 from tkinter import *
 from tkinter import filedialog
+import tempfile
 
 class SelectFile:  #class for first screen
 
 
         def __init__(self, master):  #this function creates the buttons on the first screen and points the command at the open file function
 
-            self.filebutton = Button(master, text="Open Files for Selection button", command=self.getfile)
+            self.filebutton = Button(master, text="Open File for Selection button", command=self.getfile)
             self.filebutton.pack()
 
             self.quitbutton = Button(master, text="Quit")# command=quit(mainbox))
@@ -19,7 +20,13 @@ class SelectFile:  #class for first screen
         def getfile(self):   #this is the open file function
 
             selectedfile = filedialog.askopenfilename(filetypes=[('All files', '*.*')])  #'selectedfile' is the file as an arguement
-            print(selectedfile)     #REMOVABLE TEST TO CHECK THE FILE OPENS CORRECTLY
+            selectedfile = tempfile.TemporaryFile(prefix='tempFATfile', suffix='tmp', dir="C:")
+            #print(selectedfile)               #test to confirm creation of tempfile
+
+
+        def converttobinary(self):           #function to convert file to binary text and save as a new temp file
+
+
 
 
 
@@ -29,12 +36,9 @@ mainbox.geometry("1000x600+200+50") #main box position and size
 theLabel = Label(mainbox, text="MAIN SELECT FILE SCREEN")
 theLabel.pack()  # packs the text from the previous line into the screen
 mainbox.title("File Analysis Tool")
-getfilebutton = selectfile(mainbox)
+getfilebutton = SelectFile(mainbox)
 #END - CREATE THE MAIN WINDOW
 
 
-mainbox.mainloop()  # this keeps the box open and provides the X or minimize, needs to be in the same 'def' as 'root'
+mainbox.mainloop()  # this keeps the box open and provides the X or minimize
 
-
-
-testrun = selectfile() #THIS IS A TEST RUN OF THIS CLASS
