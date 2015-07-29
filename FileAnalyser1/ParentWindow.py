656886e73bf7388477575cd27bed8592fccad7da
cavanaghb <briancavanagh99@gmail.com>
2015-07-29 11:40:32 +0100

    v4 New File Layout

--- FileAnalyser1/ParentWindow.py
+++ FileAnalyser1/ParentWindow.py
@@ -4,42 +4,45 @@ __author__ = 'cavanaghb'
 
 import tkinter
 from tkinter import filedialog
-import tempfile
-import binaryconvert
-import hexconvert
 
 
-class mainwindow(tkinter.Tk):
+class MainWindow(tkinter.Tk):           #main opening window
+
     def __init__(self, parent):
         tkinter.Tk.__init__(self, parent)
         self.parent = parent
         self.startapp()
 
-    def startapp(self):
+
+
+    def startapp(self):             #first window dialog to open
         self.grid()
 
-        filebutton = tkinter.Button(self, text="Open File for Selection button", command=self.getfile)
-        filebutton.grid(column=1, row=0)
+        filebutton = tkinter.Button(self, text="Open File for Selection button", width=500, command=self.FileOpen.getfile)
+        filebutton.grid(column=0, row=0)
 
-        quitbutton = tkinter.Button(self, text="Quit", command=quit)
-        quitbutton.grid(column=2, row=0)
+        quitbutton = tkinter.Button(self, text="Quit", width=500, command=quit)
+        quitbutton.grid(column=1, row=0)
 
         self.grid_columnconfigure(0, weight=1)
+
+
 
 
     def getfile(self):   #this is the open file function linked back to the open file button
 
         selectedfile = filedialog.askopenfilename(filetypes=[('All files', '*.*')])
 
+
         if selectedfile.endswith('.txt'):
 
             newbinaryfile = open('binaryfiletext.txt', 'w+')              #create a new file to transfer the data to
             newhexfile = open('hexfiletext.txt', 'w+')
 
-            binaryconvert.bincontxt(newbinaryfile.write(selectedfile))    #transfer the data to the new files and then convert to binary
-            hexconvert.hexcontxt(newhexfile.write(selectedfile))           #transfer the data to the new files and then convert to hex
+            newbinaryfile.write(selectedfile)
+            newhexfile.write(selectedfile)          #transfer the data to the new files and then convert to hex
 
-            printbinary = Toplevel(mainwindow)
+
 
 
         elif selectedfile.endswith('.bmp'):
@@ -55,10 +58,24 @@ class mainwindow(tkinter.Tk):
 
 #Need to create a binary and hex conversion module, this will be used to convert and save the data into the newly created files
 
+#def #TAB screen for binary
 
 
-if __name__ == "__main__":
-    NewWindow = mainwindow(None)
-    NewWindow.geometry("1000x600+200+50")
-    NewWindow.title('File Analysis Tool')
-    NewWindow.mainloop()
+#def #TAB screen for hex
+
+
+#def #screen for file analysis breakdown
+
+
+#def #screen for searching data in file
+
+
+#def #Screen n.....
+
+
+
+if __name__ == "__main__":                  #mainwindow app start
+    root = MainWindow(None)
+    root.geometry("1000x600+200+50")
+    root.title('File Analysis Tool')
+    root.mainloop()
